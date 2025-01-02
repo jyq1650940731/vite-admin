8a602584a28eee36b0e855b0a1630fd2098554fc
棋 <oncwnuN8mQQfMsttkH6GVxIhtrwE@git.weixin.qq.com>
2025-01-03 01:20:52 +0800

    chore: eslint配置

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default tseslint.config(
+  {
+    ignores: ["node_modules", "dist", "public"],
+  },
+  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  //隐藏的全局变量定义问题
+  { languageOptions: { globals: globals.browser } },
+  //eslint现成规则集
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  eslintPluginPrettierRecommended,
+);
