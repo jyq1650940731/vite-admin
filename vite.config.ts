a18beaf5449e0047909242ec3367f02963de6dec
棋 <oncwnuN8mQQfMsttkH6GVxIhtrwE@git.weixin.qq.com>
2025-01-03 21:48:47 +0800

    chore: 自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -23,10 +23,16 @@ export default defineConfig(({ mode, command }: ConfigEnv) => {
     plugins: [
       vue(),
       AutoImport({
+        imports: ["vue", "vue-router", "pinia"],
+        dts: "src/types/auto-import.d.ts",
         resolvers: [ElementPlusResolver()],
+        eslintrc: {
+          enabled: false, // 1、改为true用于生成eslint配置。2、生成后改回false，避免重复生成消耗
+        },
       }),
       Components({
         resolvers: [ElementPlusResolver()],
+        dts: "src/types/components.d.ts",
       }),
       ElementPlus({
         useSource: true,
