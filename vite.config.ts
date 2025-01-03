c41a9b89490940abb1964c795766dbafa8c41176
棋 <oncwnuN8mQQfMsttkH6GVxIhtrwE@git.weixin.qq.com>
2025-01-03 10:56:11 +0800

    chore: 环境变量配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import path from "node:path";
 
+import { defineConfig, ConfigEnv, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+const ENV_DIR = path.join(__dirname, "env");
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode, command }: ConfigEnv) => {
+  console.log(mode, command);
+  if (command === "serve") {
+    console.log(123);
+  } else {
+    console.log(123);
+  }
+  //获取环境变量所在路径
+  const env = loadEnv(mode, path.resolve(process.cwd(), "env"));
+  console.log(env);
+  return {
+    plugins: [vue()],
+    envDir: ENV_DIR,
+  };
 });
